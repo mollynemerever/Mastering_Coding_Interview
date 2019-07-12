efb510a2e926364267870dfc153a90b53a6f9e19
github username <github email address>
2019-07-12 12:31:58 -0700

    first commit

---
+++ reversestr.js
@@ -0,0 +1,17 @@
+//create function that reverses a string
+
+let str = "Hi my name is Molly";
+
+function reverseString(str) {
+  if (str.length === 0) {
+    return "invalid string";
+  }
+
+  let output = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    output += str.charAt(i);
+  }
+  return output;
+}
+
+console.log(reverseString(str));
